f5a5be934f135923979419873a1296c68e060293
Nathan Faltermeier <nefalt@att.net>
2015-10-10 22:07:48 -0500

    Project started

---
+++ TitleMenu.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class TitleMenu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleMenu extends World
+{
+
+    /**
+     * Constructor for objects of class TitleMenu.
+     * 
+     */
+    public TitleMenu()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
