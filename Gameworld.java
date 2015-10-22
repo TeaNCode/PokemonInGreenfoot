7d46b3deb5ca1e9f06761ec9f78543134626b00e
Blackop778 <nefalt@att.net>
2015-10-21 21:24:34 -0500

    Picking character's name added

--- Gameworld.java
+++ Gameworld.java
@@ -35,8 +35,20 @@ public class Gameworld extends World
     public void pickName()
     {
         setBackground("White.png");
-        for(int i = 1; i < 11;  i++) addObject(new Letter("PDisplay" + i,"a",i),75 + (i * 25),75);
-        addObject(new Letter("PName","a",0),100,150);
-        addObject(new Letter("PName","b",1),125,150);
+        for(int i = 1; i < 11;  i++) addObject(new Letter("PDisplay" + i,"a",i),53 + (i * 45),75);
+        String[] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
+            "R","S","T","U","V","W","X","Y","Z"};
+        for(int i = 0; i < 26; i++)
+        {
+            if(i < 15) addObject(new Letter("PName",letters[i],i),75 + (i * 35),150);
+            else addObject(new Letter("PName",letters[i],i),75 + ((i - 15) * 35),200);
+        }
+        addObject(new Letter("PName",",",26),460,200);
+        addObject(new Letter("PName",".",27),495,200);
+        addObject(new Letter("PName","?",28),530,200);
+        addObject(new Letter("PName","!",29),565,200);
+        for(int i = 0; i < 9; i++) addObject(new Letter("PName",Integer.toString(i + 1),30 + i),
+        75 + (i * 35),250);
+       addObject(new Letter("PName","0",39),390,250);
     }
 }
--- Letter.java
+++ Letter.java
@@ -11,14 +11,15 @@ public class Letter extends Actor
     private String letter;
     private String type;
     private int index;
-    private int first_;
+    static int charToWrite;
     public Letter(String typ,String lttr,int indx)
     {
         index = indx;
         letter = lttr;
         type = typ;
-        if(type == "PName") setImage(new GreenfootImage(letter,30,Color.BLACK,new Color(0,0,0,0)));
-        else setImage(new GreenfootImage("_",50,Color.BLACK,new Color(0,0,0,0)));
+        charToWrite = 1;
+        if(type == "PName") setImage(new GreenfootImage(letter,50,Color.BLACK,new Color(0,0,0,0)));
+        else setImage(new GreenfootImage("_",70,Color.BLACK,new Color(0,0,0,0)));
     }
 
     /**
@@ -41,7 +42,7 @@ public class Letter extends Actor
     public void updatePicture()
     {
         SaveData savedata = new SaveData();
-        setImage(new GreenfootImage(savedata.Name.substring(index - 1, index),50,Color.BLACK,new Color(0,0,0,0)));
+        setImage(new GreenfootImage(savedata.Name.substring(index - 1, index),70,Color.BLACK,new Color(0,0,0,0)));
         //System.out.println(savedata.Name);
     }
 
@@ -51,19 +52,17 @@ public class Letter extends Actor
         if(savedata.Name == "nullERRORplox") savedata.Name = letter + "_________";
         else
         {
-            first_ = 10;
-            for(int i = 1;  i < 10; i++)
+            String shortName = savedata.Name.substring(0,charToWrite);
+            if(shortName.length() < 10)
             {
-                if(savedata.Name.substring(i,i + 1) == "_")
-                {
-                    if(i < first_)
-                    {
-                        first_ = i;
-                    }
-                }
+                savedata.Name = savedata.Name.substring(0,charToWrite) + letter;
+                //savedata.Name.concat("_________");
+                savedata.Name = savedata.Name + "__________";
+                if(savedata.Name.length() > 10) savedata.Name = savedata.Name.substring(0,10);
+                //System.out.println("Printing parts of name: \n" //For debugging
+                //    + savedata.Name.substring(0,charToWrite) + " " + letter); //For debugging
+                charToWrite++;
             }
-            savedata.Name = savedata.Name.substring(0,first_) + letter;
-            while(savedata.Name.length() < 10) savedata.Name.concat("_");
         }
         //System.out.println(savedata.Name); //For debugging
     }
